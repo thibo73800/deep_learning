0ca2da6f854ea0a2bfff4aadf5176de5de5cf259
Thibault Neveu <thibo73800@gmail.com>
2018-01-12 14:51:01 +0000

    Tensorflow image classifier

--- tensorflow_image_classifier.py
+++ tensorflow_image_classifier.py
@@ -1,65 +1,56 @@
-
-# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-# Import Fashion MNIST
-fashion_mnist = input_data.read_data_sets('input/data', one_hot=True)
-# Label for Fashion MNIST
-label_dict = {
-0: "T-shirt/top",
-1: "Trouser",
-2: "Pullover",
-3: "Dress",
-4: "Coat",
-5: "Sandal",
-6: "Shirt",
-7: "Sneaker",
-8: "Bag",
-9: "Ankle boot"
-}
+mnist = input_data.read_data_sets("input/data", one_hot=True)
 
-# Print the shape of the dataset (images)
-print(fashion_mnist.train.images.shape)
-# Print the shape of the dataset (targets)
-print(fashion_mnist.train.labels.shape)
-
-# Plot one example of image
-plt.imshow(fashion_mnist.train.images[0].reshape(28, 28), cmap="gray")
-plt.title(label_dict[np.argmax(fashion_mnist.train.labels[0])])
-plt.show()
-
-# Create the graph
-# Placeholder
+# Graph Inputs
 tf_features = tf.placeholder(tf.float32, [None, 784])
 tf_targets = tf.placeholder(tf.float32, [None, 10])
 
 # Variables
-w1 = tf.Variable(tf.zeros([784, 10]))
+w1 = tf.Variable(tf.random_normal([784, 10]))
 b1 = tf.Variable(tf.zeros([10]))
 
-# Neural network operations
+#  Operations
 z1 = tf.matmul(tf_features, w1) + b1
-py = tf.nn.softmax(z1)
+softmax = tf.nn.softmax(z1)
 
-# Error and training
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=tf_targets, logits=z1)
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# Error + Train
+error = tf.nn.softmax_cross_entropy_with_logits(labels=tf_targets, logits=z1)
+train = tf.train.GradientDescentOptimizer(0.5).minimize(error)
 
 # Metrics: Accuracy
-correct_prediction = tf.equal(tf.argmax(py,1), tf.argmax(tf_targets,1))
+correct_prediction = tf.equal(tf.argmax(softmax, 1), tf.argmax(tf_targets, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+print(mnist.train.labels[0])
+
 with tf.Session() as sess:
-    # Init variables
     sess.run(tf.global_variables_initializer())
 
-    # Train the neural network
-    for _ in range(1000):
-        batch_xs, batch_ys = fashion_mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={tf_features: batch_xs, tf_targets: batch_ys})
+    # Train the model
+    epochs = 1000
+    for e in range(epochs):
+        batch_features, batch_targets = mnist.train.next_batch(100)
+        sess.run(train, feed_dict={tf_features: batch_features, tf_targets: batch_targets})
 
-    # Print the accuracy on the test set
-    print("accuracy", sess.run(accuracy, feed_dict={tf_features: fashion_mnist.test.images, tf_targets: fashion_mnist.test.labels}))
+    true_cls = []
+    py_cls = []
+    # Print the prediction for the first 10 predictions
+    for c in range(0, 10):
+        py = sess.run(softmax, feed_dict={
+            tf_features: [mnist.test.images[c]]
+        })
+        true_cls.append(np.argmax(mnist.test.labels[c]))
+        py_cls.append(np.argmax(py))
+    print("true_cls", true_cls)
+    print("py cls", py_cls)
+
+    # Accuracy on the test set
+    acc = sess.run(accuracy, feed_dict={
+        tf_features: mnist.test.images,
+        tf_targets:  mnist.test.labels
+    })
+    print("Accuracy on the test set", acc)
